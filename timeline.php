c1a56ead90c6cfd6c868b0704b12a29441e2cbe0
Glen Geldhof <glen.geldhof@hotmail.be>
2016-05-26 01:44:07 +0200

    timer en kaarten

--- timeline.php
+++ timeline.php
@@ -18,25 +18,65 @@ $votes->getVotes();
     </div>
     
     <div class="content">
-               <p class="h4 top100 marginleftright">Niet eens met de scheids, druk!</p>
+        <p class="h4 top100"><time>00:00</time></p>
 
-            <a href="uservote.php" class="knop"></a>
+
+            <a href="uservote.php"><img class="knopwhist"src="images/whis.svg"></a>
     </div>
     
     <div class="container center">  
-                
-                <div class="col-xs-3 center"><input type="button" value="<?php echo 2?>" name="red" id="red" class="card-red" /></div>
-                <div class="col-xs-3"><input type="button" value="<?php echo 4 ?>" name="yellow" id="yellow" class="card-yellow" /></div>
-                <div class="col-xs-3"><input type="button" value="<?php echo 0 ?>" name="goal" id="goal" class="card-goal" />
-                </div>
-                <div class="col-xs-3"><input type="button" value="<?php echo 0 ?>" name="offside" id="offside" class="card-offside"/></div>
-        
+                <div class="row">
+                    <div class="col-xs-2"></div>
+                <div class="col-xs-4"><input type="button" value="<?php echo 2?>" name="red" id="red" class="scard-red" /></div>
+                <div class="col-xs-4"><input type="button" value="<?php echo 4 ?>" name="yellow" id="yellow" class="scard-yellow" /></div>
+                    <div class="col-xs-2"></div>
+        </div>
     </div>
     
 </div>
 
 <?php include('templates/footer.php'); ?>
+<script>
+    var p = document.getElementsByTagName('p')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0,
+    t;
 
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+                clearTimeout(t);
+        }
+    }
+    
+    p.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+
+/* Start button */
+start.onclick = timer;
+
+/* Stop button */
+stop.onclick = function() {
+    clearTimeout(t);
+}
+
+/* Clear button */
+clear.onclick = function() {
+    p.textContent = "00:00";
+    seconds = 0; minutes = 0;
+}</script>
 
 
 
